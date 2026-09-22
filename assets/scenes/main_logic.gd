b261a881ede9f6bc82bfc256dd7f0e1a8752a0f9
nkyd-0 <angustanipad@gmail.com>
2026-09-21 20:59:04 -0700

    added blocks that work

--- assets/scenes/main_logic.gd
+++ assets/scenes/main_logic.gd
@@ -1,8 +1,25 @@
 extends Node2D
 
-var block = preload("uid://cr57ap0vqk2m8").instantiate()
 
-func _physics_process(_delta: float) -> void:
+var block = preload("uid://cr57ap0vqk2m8")
+var rng = RandomNumberGenerator.new()
+
+func _process(_delta: float) -> void:
+	if Engine.get_process_frames() % 120 == 0:
+		add_blocks()
 	
-	add_sibling(block)
+
+# create blocks
+func add_blocks():
+	var block_instance = block.instantiate()
+	get_tree().get_root().get_node(^"/root/Node2D/blocks").add_child(block_instance)
+
+	# make it so blocks are random up and down y
+	var randomizer = rng.randf_range(-550.0, 550.0)
 	
+	# set position
+	block_instance.global_transform = global_transform
+	block_instance.position = Vector2(1280.0, randomizer)
+	
+func remove_blocks():
+	pass
--- assets/scripts/player_detect.gd
+++ assets/scripts/player_detect.gd
@@ -3,7 +3,6 @@ extends StaticBody2D
 const SPEED = 300.0
 
 @onready var blocks: StaticBody2D = $"."
-var rng = RandomNumberGenerator.new()
 
 
 func _on_body_entered(body: Node) -> void:
@@ -12,7 +11,5 @@ func _on_body_entered(body: Node) -> void:
 func _physics_process(delta: float) -> void:
 	blocks.position.x -= SPEED * delta
 	
-	# make it so blocks are random up and down y
-	var randomizer = rng.randf_range(-10.0, 10.0)
 	
 	
